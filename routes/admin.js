95726c66af2cc1b928bbbeaf348570887814275b
gabizinha12 <gabrielle.plima1965@gmail.com>
2020-09-30 16:04:31 -0300

    terminado :tada:

--- routes/admin.js
+++ routes/admin.js
@@ -64,20 +64,93 @@ router.post('/produtos/novo', (req,res) => {
         preco: req.body.preco,
     }
    new Produto(novoProduto).save().then(() => {
+        req.flash("success_msg", "Produto salva com sucesso" )
        res.redirect("/admin/produtos")
    }).catch((err) => {
-    console.log(err)
+    req.flash("error_msg", "Houve um erro ao salvar")
+    res.redirect("/admin/produtos")
    })
 })
 
 router.get('/categorias/edit/:id', (req,res) => {
-    res.render("admin/editcategorias")
+    Categoria.findOne({_id: req.params.id}).lean().then((categoria) => {
+        res.render("admin/editcategorias", {categoria: categoria})
+    }).catch((err) => {
+        req.flash("error_msg", "Incapaz de editar categoria")
+        res.redirect("/admin/categorias")
+    })
 })
 
 router.get('/produtos/edit/:id', (req,res) => {
-    res.render("admin/editprodutos")
+    Produto.findOne({_id: req.params.id}).lean().then((produto) => {
+        res.render("admin/editprodutos", {produto: produto})
+    }).catch((err) => {
+        req.flash("error_msg", "Incapaz de editar produto")
+        res.redirect("/admin/produtos")
+    })
 })
 
+
+router.post('/categorias/edit', (req,res) => {
+    Categoria.findOne({_id: req.body.id}).then((categoria) => {
+        categoria.nome = req.body.nome
+        categoria.slug = req.body.slug
+          
+        categoria.save().then(() => {
+            req.flash("success_msg", "Categoria editada com sucesso")
+            res.redirect('/admin/categorias')
+        }).catch((err) => {
+            req.flash("error_msg", "Houve um erro interno ao salvar a edição")
+            res.redirect('/admin/categorias')
+
+        })
+
+
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro ao editar categoria")
+        res.redirect("/admin/categorias")
+    })
+})
+
+router.post('/produtos/edit', (req,res) => {
+    Produto.findOne({_id: req.body.id}).then((produto) => {
+        produto.nome = req.body.nome
+        produto.slug = req.body.slug
+        produto.preco = req.body.preco
+
+        produto.save().then(() => {
+            req.flash("success_msg", "Produto editado com sucesso")
+            res.redirect('/admin/produtos')
+        }).catch((err) => {
+            req.flash("error_msg", "Houve um erro interno ao salvar a edição")
+            res.redirect('/admin/produtos')
+        })
+
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro ao editar produto")
+        res.redirect("/admin/produtos")
+    })
+
+})
+
+router.post('/categorias/deletar', (req,res) => {
+    Categoria.deleteOne({_id: req.body.id}).then(() => {
+        req.flash("success_msg", "Categoria deletada com sucesso")
+        res.redirect('/admin/categorias')
+    }).catch((err) => {
+        req.flash("error_msg", "Ocorreu um erro inesperado")
+    })
+
+})
+
+router.post('/produtos/deletar', (req,res) => {
+    Produto.deleteOne({_id: req.body.id}).then(() => {
+        req.flash("success_msg", "Produto deletado com sucesso")
+        res.redirect('/admin/produtos')
+    }).catch((err) => {
+        req.flash("error_msg", "Ocorreu um erro inesperado")
+    })
+})
 
 router.get('/categorias', (req,res) => {
     Categoria.find().lean().then((categorias) => {
