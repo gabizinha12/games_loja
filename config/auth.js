5753eb36b11d641467322e64ad4e8b9bb9cd6b7c
gabizinha12 <gabrielle.plima1965@gmail.com>
2020-10-03 01:59:40 -0300

    login funfando

--- config/auth.js
+++ config/auth.js
@@ -8,7 +8,7 @@ const Usuario = mongoose.model('usuarios')
 
 
 module.exports = function(passport) {
-    passport.use(new localStrategy({usernameField: 'email', passwordField:'senha'}, (email, password, done) => {
+    passport.use(new localStrategy({usernameField: 'email', passwordField:'senha'}, (email, senha, done) => {
         Usuario.findOne({email: email}).then((usuario) => {
             if(!usuario) {
                 return done(null, false, {message: "Esta conta não existe"})
